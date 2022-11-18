0ed7fd51b326f1bf8753b9d1dd0644a0476f3b16
Chaz Corlew <chaz.corlew@gmail.com>
2022-11-18 13:56:10 -0800

    common.exception packages complete

--- src/main/java/common/Request.java
+++ src/main/java/common/Request.java
@@ -1,4 +1,8 @@
 package common;
 
-public interface Request {
+public interface Request <T> {
+
+    T extractEntity();
+
+
 }
--- src/main/java/common/ResourceCreationResponse.java
+++ src/main/java/common/ResourceCreationResponse.java
@@ -1,4 +1,29 @@
 package common;
 
 public class ResourceCreationResponse {
+    private String resourceId;
+
+    public ResourceCreationResponse(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "ResourceCreationResponse{" +
+                "resourceId='" + resourceId + '\'' +
+                '}';
+    }
+
 }
+
+
+
+
